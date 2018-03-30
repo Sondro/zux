3a3d2007bed4ec00fde7c15d5ce2ade9977e9a35
Sondro <memoiran@hotmail.com>
2018-03-30 06:18:08 -0700

    Update khafile.js

--- examples/Elements/khafile.js
+++ examples/Elements/khafile.js
@@ -2,6 +2,6 @@ let project = new Project('Elements');
 
 project.addSources('Sources');
 project.addAssets('../SharedAssets/**');
-project.addLibrary('../../../../zux');
+project.addLibrary('../../../../zui');
 
 resolve(project);
